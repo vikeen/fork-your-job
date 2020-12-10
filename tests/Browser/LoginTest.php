8af6164d26b03aeb2ec22de1b9af62029f86721e
John Rake <john.rake12@gmail.com>
2020-12-10 00:04:35 -0600

    selenium + dusk browser test

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Throwable;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testExample()
+    {
+        $user = User::factory()->create([
+            'email' => 'john.doe@gmail.com'
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->clickLink("Login")
+                ->type("email", "john.doe@gmail.com")
+                ->type("password", "password")
+                ->press("LOGIN")
+                ->assertSee("You're logged in!");
+        });
+    }
+}
